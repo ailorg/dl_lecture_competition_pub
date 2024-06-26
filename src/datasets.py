ea107bd97b1abbac46462543477e45b94b3d2d72
SeanNobel <sensho19950503@gmail.com>
2024-06-26 09:28:32 +0000

    data loading each sample

--- src/datasets.py
+++ src/datasets.py
@@ -3,36 +3,41 @@ import numpy as np
 import torch
 from typing import Tuple
 from termcolor import cprint
+from glob import glob
 
 
 class ThingsMEGDataset(torch.utils.data.Dataset):
     def __init__(self, split: str, data_dir: str = "data") -> None:
         super().__init__()
-        
         assert split in ["train", "val", "test"], f"Invalid split: {split}"
+        
         self.split = split
+        self.data_dir = data_dir
         self.num_classes = 1854
-        
-        self.X = torch.load(os.path.join(data_dir, f"{split}_X.pt"))
-        self.subject_idxs = torch.load(os.path.join(data_dir, f"{split}_subject_idxs.pt"))
-        
-        if split in ["train", "val"]:
-            self.y = torch.load(os.path.join(data_dir, f"{split}_y.pt"))
-            assert len(torch.unique(self.y)) == self.num_classes, "Number of classes do not match."
+        self.num_samples = len(glob(os.path.join(data_dir, f"{split}_X", "*.npy")))
 
     def __len__(self) -> int:
-        return len(self.X)
+        return self.num_samples
 
     def __getitem__(self, i):
-        if hasattr(self, "y"):
-            return self.X[i], self.y[i], self.subject_idxs[i]
+        X_path = os.path.join(self.data_dir, f"{self.split}_X", str(i).zfill(5) + ".npy")
+        X = torch.from_numpy(np.load(X_path))
+        
+        subject_idx_path = os.path.join(self.data_dir, f"{self.split}_subject_idxs", str(i).zfill(5) + ".npy")
+        subject_idx = torch.from_numpy(np.load(subject_idx_path))
+        
+        if self.split in ["train", "val"]:
+            y_path = os.path.join(self.data_dir, f"{self.split}_y", str(i).zfill(5) + ".npy")
+            y = torch.from_numpy(np.load(y_path))
+            
+            return X, y, subject_idx
         else:
-            return self.X[i], self.subject_idxs[i]
+            return X, subject_idx
         
     @property
     def num_channels(self) -> int:
-        return self.X.shape[1]
+        return np.load(os.path.join(self.data_dir, f"{self.split}_X", "00000.npy")).shape[0]
     
     @property
     def seq_len(self) -> int:
-        return self.X.shape[2]
+        return np.load(os.path.join(self.data_dir, f"{self.split}_X", "00000.npy")).shape[1]
