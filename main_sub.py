72f60cc9c452c1e28e609535776471109265ee3f
Minami.h <m.hotta@4nonome.com>
2024-07-01 22:26:32 +0900

    all files

--- main_sub.py
+++ main_sub.py
@@ -10,7 +10,8 @@ import torch
 import torch.nn as nn
 import torchvision
 from torchvision import transforms
-from transformers import BertTokenizer, BertModel
+# from transformers import BertTokenizer, BertModel, AutoTokenizer, AutoModelForSequenceClassification
+from transformers import BertTokenizer
 
 def set_seed(seed):
     random.seed(seed)
@@ -131,25 +132,28 @@ class VQADataset(torch.utils.data.Dataset):
         """
         image = Image.open(f"{self.image_dir}/{self.df['image'][idx]}")
         image = self.transform(image)
-        # TODO: one-hotベクトルではなくtokenizer等を利用して分散表現に(BERT?)
-        question = np.zeros(len(self.idx2question) + 1)  # 未知語用の要素を追加
-        question_words = self.df["question"][idx].split(" ")
-        # TODO: 現）訓練データに存在する回答の中から適切なものを選択するクラス形式
-        # TODO: 未）訓練データに存在しない回答もカバーできるコーパスを利用（hugging faceのclass_mapping）
-        for word in question_words:
-            try:
-                question[self.question2idx[word]] = 1  # one-hot表現に変換
-            except KeyError:
-                question[-1] = 1  # 未知語
+        # ★ one-hotベクトルではなくtokenizer等を利用して分散表現に(BERT?)
+        # question = np.zeros(len(self.idx2question) + 1)  # 未知語用の要素を追加
+        # question_words = self.df["question"][idx].split(" ")
+        question = self.df["question"][idx]
+        question_tokens = self.tokenizer(question, return_tensors='pt', padding='max_length', max_length=20, truncation=True)
+        
+        #for word in question_words:
+        #    try:
+        #        question[self.question2idx[word]] = 1  # one-hot表現に変換
+        #    except KeyError:
+        #        question[-1] = 1  # 未知語
 
         if self.answer:
             answers = [self.answer2idx[process_text(answer["answer"])] for answer in self.df["answers"][idx]]
             mode_answer_idx = mode(answers)  # 最頻値を取得（正解ラベル）
 
-            return image, torch.Tensor(question), torch.Tensor(answers), int(mode_answer_idx)
+            # return image, torch.Tensor(question), torch.Tensor(answers), int(mode_answer_idx)
+            return image, question_tokens['input_ids'].squeeze(), question_tokens['attention_mask'].squeeze(), torch.Tensor(answers), int(mode_answer_idx)
 
         else:
-            return image, torch.Tensor(question)
+            # return image, torch.Tensor(question)
+            return image, question_tokens['input_ids'].squeeze(), question_tokens['attention_mask'].squeeze()
 
     def __len__(self):
         return len(self.df)
@@ -175,7 +179,7 @@ def VQA_criterion(batch_pred: torch.Tensor, batch_answers: torch.Tensor):
     return total_acc / len(batch_pred)
 
 
-# 3. モデルのの実装
+# 3. モデルの実装
 # ResNetを利用できるようにしておく
 class BasicBlock(nn.Module):
     expansion = 1
@@ -292,25 +296,26 @@ def ResNet50():
 
 
 class VQAModel(nn.Module):
-    def __init__(self, vocab_size: int, n_answer: int):
+    def __init__(self, n_answer: int):
         super().__init__()
-        self.resnet = ResNet18()
+        self.resnet = ResNet50()
         # self.text_encoder = nn.Linear(vocab_size, 512)
         # ★追加：BERTに変更
         self.text_encoder = BertModel.from_pretrained('bert-base-uncased')
 
         self.fc = nn.Sequential(
             nn.Linear(1024, 512),
+            # nn.Dropout(0.25), # TODO: dropout層追加する？
             nn.ReLU(inplace=True),
             # nn.Dropout(0.25), # TODO: dropout層追加する？
             nn.Linear(512, n_answer)
         )
 
-    def forward(self, image, question):
+    def forward(self, image, question_input_ids, question_attention_mask):
         image_feature = self.resnet(image)  # 画像の特徴量
-        question_feature = self.text_encoder(question)  # テキストの特徴量
-        # text_feature = self.text_encoder(input_ids=input_ids, attention_mask=attention_mask).pooler_output # question_featureの代わり？
-        x = torch.cat([image_feature, question_feature], dim=1)
+        # question_feature = self.text_encoder(question)  # テキストの特徴量
+        text_feature = self.text_encoder(input_ids=question_input_ids, attention_mask=question_attention_mask).pooler_output # question_featureの代わり？
+        x = torch.cat([image_feature, text_feature], dim=1)
         x = self.fc(x)
 
         return x
@@ -328,9 +333,9 @@ def train(model, dataloader, optimizer, criterion, device, scheduler, clip_value
     
     for image, question, answers, mode_answer in dataloader:
         image, question, answer, mode_answer = \
-            image.to(device), question.to(device), answers.to(device), mode_answer.to(device)
+            image.to(device), question.to(device), question_input_ids.to(device), question_attention_mask.to(device), answers.to(device), mode_answer.to(device)
 
-        pred = model(image, question)
+        pred = model(image, question_input_ids, question_attention_mask)
         loss = criterion(pred, mode_answer.squeeze())
 
         optimizer.zero_grad()
@@ -393,7 +398,8 @@ def main():
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
 
     model = VQAModel(vocab_size=len(train_dataset.question2idx)+1, n_answer=len(train_dataset.answer2idx)).to(device)
-
+    # model = VQAModel(n_answer=len(train_dataset.answer2idx)).to(device)?
+    
     # optimizer / criterion
     num_epoch = 20
     criterion = nn.CrossEntropyLoss()
@@ -407,7 +413,7 @@ def main():
     
     # train model
     for epoch in range(num_epoch):
-        train_loss, train_acc, train_simple_acc, train_time = train(model, train_loader, optimizer, criterion, device)
+        train_loss, train_acc, train_simple_acc, train_time = train(model, train_loader, optimizer, criterion, device, scheduler)
         # eval_loss, eval_acc, eval_simple_acc, eval_time = eval(model, test_loader, optimizer, criterion, device)
         # ★追加：eval 関数の追加はしなくていいの？
         print(f"【{epoch + 1}/{num_epoch}】\n"
@@ -430,9 +436,12 @@ def main():
     # 提出用ファイルの作成
     model.eval()
     submission = []
-    for image, question in test_loader:
-        image, question = image.to(device), question.to(device)
-        pred = model(image, question)
+    # for image, question in test_loader:
+        # image, question = image.to(device), question.to(device)
+        # pred = model(image, question)
+    for image, question_input_ids, question_attention_mask in test_loader:
+        image, question_input_ids, question_attention_mask = image.to(device), question_input_ids.to(device), question_attention_mask.to(device)
+        pred = model(image, question_input_ids, question_attention_mask)
         pred = pred.argmax(1).cpu().item()
         submission.append(pred)
 
